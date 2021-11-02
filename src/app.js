86ca61930621a56c8b04838699612fc6a15a1b4f
Taylor Hart <thart003@ucr.edu>
2021-11-02 18:07:46 +0000

    create main

--- src/app.js
+++ src/app.js
@@ -1,11 +1,63 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let possibles = {
+  pronoun: [
+    "mine",
+    "his",
+    "you",
+    "we",
+    "theirs",
+    "I",
+    "hers",
+    "all",
+    "somebody"
+  ],
+  adj: [
+    "charming",
+    "conniving",
+    "conspiratorial",
+    "duplicitous",
+    "melancholy",
+    "brash",
+    "reflexive",
+    "doting",
+    "extemperaneous",
+    "vital",
+    "dextrous"
+  ],
+  noun: [
+    "aardvark",
+    "hippo",
+    "gypsy",
+    "turtle",
+    "whale",
+    "streetlight",
+    "government",
+    "president",
+    "congresswoman",
+    "emperor",
+    "minister",
+    "creator",
+    "comedian",
+    "alcoholic",
+    "addict",
+    "Lithuanian"
+  ]
 };
+let possible_domains = [];
+function domainCreator() {
+  for (let i = 0; i < possibles.pronoun.length; i++) {
+    for (let j = 0; j < possibles.adj.length; j++) {
+      for (let k = 0; k < possibles.noun.length; k++) {
+        possible_domains.push(
+          " " +
+            possibles.pronoun[i] +
+            possibles.adj[i] +
+            possibles.noun[i] +
+            ".com"
+        );
+      }
+    }
+  }
+}
+
+domainCreator();
+document.write(possible_domains);
